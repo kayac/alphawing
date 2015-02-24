652459c6eb14fb980ef1322f49e0cec6d7ca91ef
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2015-02-24 23:47:12 +0900

    add plist model

---
+++ app/models/plist.go
@@ -0,0 +1,55 @@
+package models
+
+import "github.com/DHowett/go-plist"
+
+const (
+	AssetKind                = "software-packege"
+	MetadataBundleIdentifier = "com.example.test"
+	MetadataKind             = "software"
+)
+
+type Plist struct {
+	Items []*Item `plist:"items"`
+}
+
+type Item struct {
+	Assets   []*Asset  `plist:"assets"`
+	Metadata *Metadata `plist:"metadata"`
+}
+
+type Asset struct {
+	Kind string `plist:"kind"`
+	Url  string `plist:"url"`
+}
+
+type Metadata struct {
+	BundleIdentifier string `plist:"bundle-identifier"`
+	BundleVersion    string `plist:"bundle-version"`
+	Kind             string `plist:kind`
+	Title            string `plist:title`
+}
+
+func NewPlist(title, version, ipaUrl string) *Plist {
+	return &Plist{
+		Items: []*Item{
+			&Item{
+				Assets: []*Asset{
+					&Asset{
+						Kind: AssetKind,
+						Url:  ipaUrl,
+					},
+				},
+				Metadata: &Metadata{
+					BundleIdentifier: MetadataBundleIdentifier,
+					BundleVersion:    version,
+					Kind:             MetadataKind,
+					Title:            title,
+				},
+			},
+		},
+	}
+}
+
+func (p *Plist) Marshall() ([]byte, error) {
+	return plist.MarshalIndent(p, plist.XMLFormat, "\t")
+}
