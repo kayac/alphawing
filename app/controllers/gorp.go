85313692011a9fe4e0bfff392de44797d5dfce83
Ichinose Shogo <shogo82148@gmail.com>
2015-02-25 00:14:53 +0900

    remove begin/commit/rollback intercept method

--- app/controllers/gorp.go
+++ app/controllers/gorp.go
@@ -88,34 +88,3 @@ func Transact(f func(gorp.SqlExecutor) error) error {
 	txn = nil
 	return nil
 }
-
-func (c *GorpController) Begin() revel.Result {
-	txn, err := Dbm.Begin()
-	if err != nil {
-		panic(err)
-	}
-	c.Txn = txn
-	return nil
-}
-
-func (c *GorpController) Commit() revel.Result {
-	if c.Txn == nil {
-		return nil
-	}
-	if err := c.Txn.Commit(); err != nil && err != sql.ErrTxDone {
-		panic(err)
-	}
-	c.Txn = nil
-	return nil
-}
-
-func (c *GorpController) Rollback() revel.Result {
-	if c.Txn == nil {
-		return nil
-	}
-	if err := c.Txn.Rollback(); err != nil && err != sql.ErrTxDone {
-		panic(err)
-	}
-	c.Txn = nil
-	return nil
-}
