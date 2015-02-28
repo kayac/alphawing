d1f35d6d0b027bccea1e86c63dca8ce617e7dd1c
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2015-02-28 19:05:20 +0900

    エラーはログに残して 404 を返す

--- app/controllers/limitedtime.go
+++ app/controllers/limitedtime.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/kayac/alphawing/app/models"
-	"github.com/kayac/alphawing/app/routes"
 	"github.com/revel/revel"
 )
 
@@ -62,34 +61,38 @@ func (c *LimitedTimeController) GetDownloadIpa(bundleId int) revel.Result {
 
 func (c *LimitedTimeController) CheckValidLimitedTimeToken() revel.Result {
 	bundle := c.Bundle
-	if c.Bundle == nil {
-		return c.NotFound("NotFound")
+
+	if bundle == nil {
+		revel.ERROR.Printf("Bundle is not found.")
+		return c.NotFound("")
 	}
 
 	token := c.Params.Query.Get(models.TokenKey)
 	seed := c.Params.Query.Get(models.SeedKey)
 	limit := c.Params.Query.Get(models.LimitKey)
 
-	c.Validation.Required(token).Message(models.TokenKey + " is required")
-	c.Validation.Required(seed).Message(models.SeedKey + " is required")
-	c.Validation.Required(limit).Message(models.LimitKey + " is required")
+	c.Validation.Required(token)
+	c.Validation.Required(seed)
+	c.Validation.Required(limit)
 	if c.Validation.HasErrors() {
-		c.Validation.Keep()
-		c.FlashParams()
-		return c.Redirect(routes.BundleControllerWithValidation.GetDownloadBundle(bundle.Id))
+		revel.ERROR.Printf("Parameters are invalid.")
+		return c.NotFound("")
 	}
 
 	tokenInfo, err := models.NewLimitedTimeTokenInfo(token, seed, limit)
 	if err != nil {
-		panic(err)
+		revel.ERROR.Printf(err.Error())
+		return c.NotFound("")
 	}
 
 	ok, err := tokenInfo.IsValid(Conf.Secret)
 	if err != nil {
-		panic(err)
+		revel.ERROR.Printf(err.Error())
+		return c.NotFound("")
 	}
 	if !ok {
-		return c.Forbidden("Forbidden")
+		revel.ERROR.Printf("Token is invalid.")
+		return c.NotFound("")
 	}
 
 	return nil
@@ -98,25 +101,26 @@ func (c *LimitedTimeController) CheckValidLimitedTimeToken() revel.Result {
 func (c *LimitedTimeController) CheckNotFound() revel.Result {
 	bundleIdStr := c.Params.Get("bundleId")
 
-	c.Validation.Required(bundleIdStr).Message("BundleId is required.")
+	c.Validation.Required(bundleIdStr)
 	if c.Validation.HasErrors() {
-		c.Validation.Keep()
-		c.FlashParams()
-		return c.Redirect(routes.AlphaWingController.Index())
+		revel.ERROR.Printf("BundleId is required.")
+		return c.NotFound("")
 	}
 
 	bundleId, err := strconv.Atoi(bundleIdStr)
 	if err != nil {
-		c.Flash.Error("BundleId is invalid.")
-		return c.Redirect(routes.AlphaWingController.Index())
+		revel.ERROR.Printf(err.Error())
+		return c.NotFound("")
 	}
 
 	bundle, err := models.GetBundle(Dbm, bundleId)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			return c.NotFound("Bundle is not found.")
+			revel.ERROR.Printf("Bundle is not found.")
+		} else {
+			revel.ERROR.Printf(err.Error())
 		}
-		panic(err)
+		return c.NotFound("")
 	}
 	c.Bundle = bundle
 
